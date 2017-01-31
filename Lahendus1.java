b0cb48943cfaa761ac7feff28608452c04d563a7
kirke <kirke@greeny.cs.tlu.ee>
2017-01-31 15:45:48 +0200

    lahendus1

---
+++ Lahendus1.java
@@ -0,0 +1,13 @@
+public class Lahendus1{
+	public static void trykiA(){
+		System.out.println("*")
+		System.out.println("*")
+		System.out.println("*")
+		System.out.println("*")
+		System.out.println("*")
+	}
+	public static void main(String[]){
+		trykiA();
+		trykiA();
+	}
+}
