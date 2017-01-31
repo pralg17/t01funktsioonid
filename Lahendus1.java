465732293cb9b792f6b97da4d6c345986502da7f
anusada <anusada@greeny.cs.tlu.ee>
2017-01-31 15:28:25 +0200

    lala

---
+++ Lahendus1.java
@@ -0,0 +1,10 @@
+public class Lahendus1{
+	public static void trykiA(){
+		System.out.println(" * ")
+		System.out.println(" * ")
+		System.out.println(" * ")
+		System.out.println(" * ")
+		System.out.println(" * ")
+	}
+	public static void main(String[] )
+}
