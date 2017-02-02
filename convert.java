b307eb12f401e3e540290955fb0b7735f161c23f
Joosep Juhanson <Juhanson@users.noreply.github.com>
2017-02-02 09:19:18 +0200

    Wrote a function to convert cm to inches

---
+++ convert.java
@@ -0,0 +1,13 @@
+public class convert{
+	//Programm laseb kasutajal sisestada sentimeetrid ja konverteerib need tollideks.
+	public static void main(String[] arg){
+		//System.out.println("Sisesta mitu sentimeetrit soovite tesieldada tollideks");
+		System.out.println(Integer.parseInt(arg[0]) * 0.393700787);
+	}
+}
+
+/*
+[joosjuha@greeny t01funktsioonid]$ java convert 30
+11.81102361
+
+*/
