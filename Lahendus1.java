d2fbbdc5b67be53f0ed70d1f561a86a40135b9f2
heinparn <heinparn@greeny.cs.tlu.ee>
2017-02-02 09:10:49 +0200

    Lisasin mittetöötava funktsiooni

--- Lahendus1.java
+++ Lahendus1.java
@@ -1,13 +1,11 @@
 public class Lahendus1{
-	public static void trykiA(){
-		System.out.println("  *  ");
-		System.out.println(" * * ");
-		System.out.println("*   *");
-		System.out.println("*****");
-		System.out.println("*   *");
-	}
-	public static void main(String[] args){
-		trykiA();
-		trykiA();
-	}
+	public static int minFunction(int n1, int n2) {
+   int min;
+   if (n1 > n2)
+      min = n2;
+   else
+      min = n1;
+
+   return min; 
+  }
 }
