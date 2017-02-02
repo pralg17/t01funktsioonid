64aeb7e63a21518f04f9567f30cfd92bec1c63d5
ingomagi <ingomagi@greeny.cs.tlu.ee>
2017-02-02 09:18:18 +0200

    made a random gen

--- Lahendus1.java
+++ Lahendus1.java
@@ -1,13 +1,11 @@
 public class Lahendus1{
 	public static void trykiA(){
-		System.out.println("  *  ");
-		System.out.println(" * * ");
-		System.out.println("*   *");
-		System.out.println("*****");
-		System.out.println("*   *");
+			for (int i = 0; i < 10; i++) {
+			int random = (int)(Math.random() * 11);
+			System.out.print(random);
+		}
 	}
 	public static void main(String[] args){
-		trykiA();
 		trykiA();
 	}
 }
