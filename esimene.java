4a0d2a18921c3dc4e70f0bd34cb81c9270cfbeec
karlbork <karlbork@greeny.cs.tlu.ee>
2017-02-02 09:18:10 +0200

    esimene funktsioon

---
+++ esimene.java
@@ -0,0 +1,16 @@
+public class esimene {
+	static int arvud(int arv1, int arv2){
+		return arv1 * arv2;
+	}
+	
+	public static void main(String[] arg) {
+		System.out.println(arvud(3, 4));
+	}
+	
+}
+
+/*
+[karlbork@greeny t01funktsioonid]$ javac esimene.java
+[karlbork@greeny t01funktsioonid]$ java esimene
+12
+*/
