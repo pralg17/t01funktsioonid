40148f58c1f7d1c765b223c63b447b4840246b87
heinparn <heinparn@greeny.cs.tlu.ee>
2017-02-02 09:20:54 +0200

    Lisasin miinimumarvu leidva funktsiooni

--- Lahendus1.java
+++ Lahendus1.java
@@ -1,11 +1,21 @@
-public class Lahendus1{
-	public static int minFunction(int n1, int n2) {
-   int min;
-   if (n1 > n2)
-      min = n2;
-   else
-      min = n1;
+public class Lahendus1 {
+   
+   public static void main(String[] args) {
+      int a = 21;
+      int b = 4;
+      int c = minArv(a, b);
+      System.out.println("Miinimumarv = " + c);
+   }
 
-   return min; 
-  }
+   /** returns the minimum of two numbers */
+   public static int minArv(int n1, int n2) {
+      int min;
+      if (n1 > n2)
+         min = n2;
+      else
+         min = n1;
+
+      return min; 
+   }
+  
 }
