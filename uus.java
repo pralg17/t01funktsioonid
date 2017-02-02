bd56c1045f58d5d24da3f13dc152473309d0c9ed
riharyys <riharyys@greeny.cs.tlu.ee>
2017-02-02 09:20:59 +0200

    uus.java lisatud

---
+++ uus.java
@@ -0,0 +1,13 @@
+import java.util.Scanner; // et kasutaja saaks arve lisada, input
+public class uus{
+	public static void main(String args[])
+	   {
+		  int x, y, z;
+		  System.out.println("Kirjuta kaks arvu, mida tahad liita: ");
+		  Scanner in = new Scanner(System.in); // Scanner, et saaks katte inputi kasutaja kaest
+		  x = in.nextInt();
+		  y = in.nextInt();
+		  z = x + y;
+		  System.out.println("Arvute summa on: "+z);
+	   }
+}
