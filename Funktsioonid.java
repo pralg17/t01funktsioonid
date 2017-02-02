57a3a97a74c76f0a144253d090f6b770d3c089ac
tanelm <tanelmaasalu@gmail.com>
2017-02-02 09:28:46 +0200

    Kodutöö nr 1 Tanel Maasalu

---
+++ Funktsioonid.java
@@ -0,0 +1,10 @@
+public class Funktsioonid{
+	
+	static double miilideks(double kilomeetrid){
+		return kilomeetrid/1.61;
+	}
+	
+	public static void main(String[] args){
+		System.out.println(miilideks(0.5));
+	}
+}
