20255ea80af135048e41a390faaedd0376553e3b
oaheinla <oaheinla@greeny.cs.tlu.ee>
2017-02-02 09:12:02 +0200

    korrutamine kahega

--- Lahendus1.java
+++ Lahendus1.java
@@ -1,13 +1,9 @@
 public class Lahendus1{
-	public static void trykiA(){
-		System.out.println("  *  ");
-		System.out.println(" * * ");
-		System.out.println("*   *");
-		System.out.println("*****");
-		System.out.println("*   *");
+	
+	static double korrutamine2(double arv){
+		return arv*2;
 	}
-	public static void main(String[] args){
-		trykiA();
-		trykiA();
+	public static void main(String[] arg){		
+		System.out.println(korrutamine2(6));				
 	}
 }
