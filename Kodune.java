138c42f416fb552a08e05daed8e6a6a1e175161c
marirein <marirein@greeny.cs.tlu.ee>
2017-02-02 09:10:47 +0200

    ristküliku pindala

---
+++ Kodune.java
@@ -0,0 +1,12 @@
+public class Kodune{
+	
+	static double ristkylik(double pikkus, double laius){
+		
+		return pikkus*laius;
+	}
+	
+	
+	public static void main(String[] args){
+		System.out.println(ristkylik(2, 4));
+	}
+}
