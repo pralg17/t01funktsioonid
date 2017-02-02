61b8c4d00db49c0fade1ad1eeb1f47439ef9a76e
rasmusaaviste <rasmusaaviste@users.noreply.github.com>
2017-02-02 09:08:31 +0200

    added homework

---
+++ Kodune.java
@@ -0,0 +1,9 @@
+public class Kodune{
+	static double kilomeetriteks(double meetrid){
+		return meetrid/1000;
+	}
+	
+	public static void main(String[] args){
+		System.out.println(kilomeetriteks(2500));
+	}
+}
