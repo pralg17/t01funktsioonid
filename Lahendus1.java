c833c6b6671494b56d40b2dcbf20acc0af9e4f84
markkoll <markkoll@greeny.cs.tlu.ee>
2017-02-02 09:19:15 +0200

    Näitab peale koma

--- Lahendus1.java
+++ Lahendus1.java
@@ -1,13 +1,28 @@
-public class Lahendus1{
-	public static void trykiA(){
-		System.out.println("  *  ");
-		System.out.println(" * * ");
-		System.out.println("*   *");
-		System.out.println("*****");
-		System.out.println("*   *");
+ import java.text.DecimalFormat;
+ 
+ 
+ public class Lahendus1{
+	 
+	 
+	public static void main(String[] arg){
+		 
+		System.out.println(naita(5.014565846546546546464646466, 3));
+	 } 
+			
+			
+	
+	public static String naita(double arv, double pealeKoma){
+		
+		
+		String argument = "#.";
+		for(int i=0; i<pealeKoma; i=i+1){
+			argument = argument + "#";	
+		}
+		
+		DecimalFormat df = new DecimalFormat(argument);
+		String format = df.format(arv);
+		return format;
 	}
-	public static void main(String[] args){
-		trykiA();
-		trykiA();
-	}
-}
+		
+ }
+	
