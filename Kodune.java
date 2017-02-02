ab7463d95eeff0d02d25ef088a6cfb8cef968ea7
rasmusaaviste <rasmusaaviste@users.noreply.github.com>
2017-02-02 09:12:16 +0200

    added another function

--- Kodune.java
+++ Kodune.java
@@ -3,7 +3,12 @@ public class Kodune{
 		return meetrid/1000;
 	}
 	
+	static double meetriteks(double kilomeetrid){
+		return kilomeetrid*1000;
+	}
+	
 	public static void main(String[] args){
 		System.out.println(kilomeetriteks(2500));
+		System.out.println(meetriteks(3.4));
 	}
 }
