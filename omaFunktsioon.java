d7fd4514154433b87f10112608e5b46398641e75
stivsire <stivsire@greeny.cs.tlu.ee>
2017-02-03 15:10:17 +0200

    Lisatud ruudu pindala funktsioon

---
+++ omaFunktsioon.java
@@ -0,0 +1,8 @@
+public class omaFunktsioon{
+	public static double ruudupindala(double kylg){
+		return Math.pow(kylg, 2);
+	}
+	public static void main(String[] args){		
+		System.out.println(ruudupindala(4));
+	}
+}
