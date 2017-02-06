f4f3bb60d5425ed1e45a67cc44aadfc765d3ae3a
henrysavi <hensav@users.noreply.github.com>
2017-02-06 13:53:20 +0200

    kodune töö

--- gear_ratio.java
+++ gear_ratio.java
@@ -4,12 +4,18 @@
 /*Jalgratta ülekandearvu kalkulaator*/
 public class gear_ratio{
 
+public static float ratio(float ratas1,float ratas2){
+    return ratas1/ratas2;
+}
+
     public static void main(String[] arg) {
 
         if (arg.length > 1){
-            System.out.println(Float.parseFloat(arg[0])/Float.parseFloat(arg[1]));
+            System.out.println(ratio(Float.parseFloat(arg[0]),Float.parseFloat(arg[1])));
         }else{
             System.out.println("Sisesta arvud kujul:java ratio 40 14");
         }
     }
+
+
 }
