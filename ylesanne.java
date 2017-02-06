e5a82d40bbb12fb857d7bbe10e202b62340bdcc2
ukupode <ukupode@greeny.cs.tlu.ee>
2017-02-06 18:20:33 +0200

    funktsiooniga variant

--- ylesanne.java
+++ ylesanne.java
@@ -1,26 +1,21 @@
 public class ylesanne{
 	//arvutab vanuse
-	public static void main(String[] arg){
+	public static String vanus(int synniaasta){
 		int aasta=2017;
-		System.out.println("Tere.");
-		if(arg.length==1){
-		  //veapüünis try-catch ka juurde
-		try{
-		   System.out.println("Te olete juba "+(aasta-Integer.parseInt(arg[0]))+" või saate see aasta.");
-		} catch(NumberFormatException e){
-		   System.out.println("Arvuteisendus ebaõnnestus");
-		}
-	  } else {
-		  System.out.println("Kasuta kujul:java ylesanne 1995");		  
-	  }
+		return ("Te olete juba "+(aasta-synniaasta)+" või saate see aasta.");
+	}
+	public static void main(String[] args){
+		
+		System.out.println(vanus(1995));
+		
 	}
 }
 
 /*
 
-[ukupode@greeny t01funktsioonid]$ java ylesanne 1995
-Tere.
+[ukupode@greeny t01funktsioonid]$ java ylesanne
 Te olete juba 22 või saate see aasta.
+
 
 
 
