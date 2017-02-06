fd116f7fb5f77b46e1d31357d06cfbbc0c8ee88a
ukupode <ukupode@greeny.cs.tlu.ee>
2017-02-06 13:01:28 +0200

    arendus

--- ylesanne.java
+++ ylesanne.java
@@ -1,14 +1,27 @@
 public class ylesanne{
-	//lisage alamprogramm mis arvutab sentimeetrid tollideks
-	public static void main(String[]args){
+	//arvutab vanuse
+	public static void main(String[] arg){
+		int aasta=2017;
 		System.out.println("Tere.");
+		if(arg.length==1){
+		  //veapüünis try-catch ka juurde
+		try{
+		   System.out.println("Te olete juba "+(aasta-Integer.parseInt(arg[0]))+" või saate see aasta.");
+		} catch(NumberFormatException e){
+		   System.out.println("Arvuteisendus ebaõnnestus");
+		}
+	  } else {
+		  System.out.println("Kasuta kujul:java ylesanne 1995");		  
+	  }
 	}
 }
 
 /*
 
-[ukupode@greeny t01funktsioonid]$ java ylesanne
+[ukupode@greeny t01funktsioonid]$ java ylesanne 1995
 Tere.
+Te olete juba 22 või saate see aasta.
+
 
 
 */
