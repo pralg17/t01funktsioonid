9b8fb53d155e3002893ad72f03a6c286a4fe888d
Toolsten <Toolsten@users.noreply.github.com>
2017-02-06 12:25:32 +0200

    Add files via upload

---
+++ Funktsioonid.java
@@ -0,0 +1,18 @@
+public class Funktsioonid{
+	
+	static double Korrutus(double arv, double aste){
+		
+		return arv*Math.pow(arv,aste);
+	}
+	
+	
+	
+	public static void main(String[] args){
+		System.out.println(Korrutus(5,5));
+		
+	}
+	
+	
+	
+	
+}
