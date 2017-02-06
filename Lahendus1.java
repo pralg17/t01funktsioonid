d2e518eab2ddbe8a33cb8b72018cfdf4d5b9322f
anusada <anusada@greeny.cs.tlu.ee>
2017-02-06 19:10:12 +0200

    added function

--- Lahendus1.java
+++ Lahendus1.java
@@ -1,13 +1,25 @@
+/*
+Leiab kolmnurga pindala kasutades k6iki kolme kylge
+*/
+import java.util.Scanner; 
 public class Lahendus1{
-	public static void trykiA(){
-		System.out.println(" * ")
-		System.out.println(" * ")
-		System.out.println(" * ")
-		System.out.println(" * ")
-		System.out.println(" * ")
+	public static float arvutus(float a, float b, float c){
+		float umbermoot = a + b + c;
+		float p = umbermoot/2;
+		float s = (p-a)*(p-b)*(p-c);
+		return s;
 	}
+
 	public static void main(String[] args){
-		trykiA();
-		trykiA();
+		float a, b, c;
+ 		Scanner scan = new Scanner(System.in);
+ 		System.out.print("Sisesta esimene kulg [cm]: ");
+ 		a = scan.nextFloat();
+ 		System.out.print("Sisesta teine kulg [cm]: ");
+ 		b = scan.nextFloat();
+ 		System.out.print("Sisesta kolmas kulg [cm]: ");
+ 		c = scan.nextFloat();
+ 		float s = arvutus(a, b, c);
+ 		System.out.println("Kolmnurga pindala on "+(Math.round(Math.sqrt(s)))+" ruutsentimeetrit");
 	}
 }
