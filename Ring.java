27eabb06159ad798fed2f9d76263ad67b8c1a481
Kristjan Kand <krisjan.kand.002@gmail.com>
2017-02-06 20:44:08 +0200

    Kodutoo

---
+++ Ring.java
@@ -0,0 +1,13 @@
+/** 
+*PROGRAMM ARVUTAB RINGI PINDALA
+*/
+
+public class Ring{
+	public static void main(String[] args){
+		int raadius = 3;
+		double pindala = 3.14159 * (raadius * raadius);
+		System.out.println("Ringi pindala on umbkaudu " + pindala);
+		
+		
+	}
+}
