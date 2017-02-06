ef321bdbf5cc24ac129e6cdb025bb01168494c55
atsklem <atsklem@greeny.cs.tlu.ee>
2017-02-06 22:43:07 +0200

    Added homework

--- Lahendus1.java
+++ Lahendus1.java
@@ -1,12 +1,30 @@
-public class Lahendus1{
-    public static void trykiA(){
-    System.out.println("'");
-    System.out.println("'");
-    System.out.println("'");
-    System.out.println("'");
-    }
-    public static void main(String[] args){
-    trykiA();
-    trykiA();
-    }
+//Näitab algarve 1-100
+
+public class Lahendus1 {
+   public static void main (String[] args)
+   {		
+       int i =0;
+       int num =0;
+       //Empty String
+       String  primeNumbers = "";
+
+       for (i = 1; i <= 100; i++)         
+       { 		  	  
+          int counter=0; 	  
+          for(num =i; num>=1; num--)
+	  {
+             if(i%num==0)
+	     {
+ 		counter = counter + 1;
+	     }
+	  }
+	  if (counter ==2)
+	  {
+	     //Appended the Prime number to the String
+	     primeNumbers = primeNumbers + i + " ";
+	  }	
+       }	
+       System.out.println("Prime numbers from 1 to 100 are :");
+       System.out.println(primeNumbers);
+   }
 }
