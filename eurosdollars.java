ca10be267e831281e1e93b3971e5884c29d9824d
kentloog <kentloog@greeny.cs.tlu.ee>
2017-02-06 17:57:51 +0200

    1. kodutöö

---
+++ eurosdollars.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class eurosdollars{
+	static double dollariteks(double eurod){
+		return eurod*1.04;
+	}
+	static double naeladeks(double eurod){
+		return eurod*0.84;
+	}
+	static double rubladeks(double eurod){
+		return eurod*63.37;
+	}
+	
+	public static void main(String[] arg){
+		Scanner reader = new Scanner(System.in);
+		
+		System.out.println("Mitu eurot teil on?");
+		float eurod = reader.nextInt();
+		double dollarid=dollariteks(eurod);
+		double naelad=naeladeks(eurod);
+		double rublad=rubladeks(eurod);
+		System.out.println(eurod+" eurot on "+dollarid+" dollarit, "+naelad+" naela ja "+rublad+" rubla");
+		
+	}
+	
+}
