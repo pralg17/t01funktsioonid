196b0cdcba74c5cdf536e5fcc7771021d9be2a79
kirke gross <kirkegross@users.noreply.github.com>
2017-02-07 13:00:56 +0200

    kodutoo

--- Lahendus1.java
+++ Lahendus1.java
@@ -1,13 +1,20 @@
+import java.util.Scanner;
 public class Lahendus1{
-	public static void trykiA(){
-		System.out.println("*")
-		System.out.println("*")
-		System.out.println("*")
-		System.out.println("*")
-		System.out.println("*")
+	public static float ristkylikuS(float a, float b){
+		float S = a * b;
+		return S;
 	}
-	public static void main(String[]){
-		trykiA();
-		trykiA();
+		
+		
+	
+	public static void main(String[] args){
+		float a, b;
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Sisesta ristkyliku esimene kylg[cm]: ");
+		a = scan.nextFloat();
+  		System.out.print("Sisesta teine kylg [cm]: ");
+  		b = scan.nextFloat();
+        float S = ristkylikuS(a, b);
+  		System.out.println("Ristkyliku pindala on "+S+" ruutsentimeetrit");
 	}
 }
