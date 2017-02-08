4633f1260dc8b11d684f0335da655b9ca5a6cad2
egenoor <egenoor@github.com>
2017-02-08 17:22:56 +0200

    kodutoo

---
+++ kodutoo1.java
@@ -0,0 +1,26 @@
+public class kodutoo1{
+	public static void main(String[] args){
+        int number1 = 10;
+        int number2 = 5;
+
+        //calculating number1 + number2;
+        int summa = number1 + number2;
+
+        //calculating number1 - number2;
+        int vahe = number1 - number2;
+
+        //calculating number1 * number2;
+        int korrutis = number1 * number2;
+
+        //calculating number1 / number2;
+        int jagatis = number1 / number2;
+
+        //Displaying the values
+        System.out.println("number1 : "+number1);
+        System.out.println("number2 : "+number2);
+        System.out.println("summa : "+summa);
+        System.out.println("vahe : "+vahe);
+        System.out.println("korrutis : "+korrutis);
+        System.out.println("jagatis : "+jagatis);
+    }
+}
