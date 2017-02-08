49cf1e651a909508a8a7b48507b83bf86c2775df
gittkaus <gittkaus@greeny.cs.tlu.ee>
2017-02-08 14:17:25 +0200

    kodune

---
+++ hypotenuse.java
@@ -0,0 +1,9 @@
+public class hypotenuse {
+ 
+    public static void main(String[] args) {
+         
+        int x = 10;
+        int y = 20;
+        System.out.println("Hypotenuse lenght is: "+Math.hypot(x, y));
+    }
+}
