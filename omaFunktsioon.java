f2e99165b93e6ca749b5279f84ce0baca5db8f4b
stivsire <stivsire@greeny.cs.tlu.ee>
2017-02-10 12:36:05 +0200

    lisatud teisendus ruutmeetriteks

--- omaFunktsioon.java
+++ omaFunktsioon.java
@@ -1,16 +1,27 @@
 public class omaFunktsioon{
-	public static int tvPindala(int korgus, int laius, int sygavus){
-		int vastusKyljed, vastusEesTaga, vastus;
+	public static double tvPindala(double korgus, double laius, double sygavus){
+		double vastusKyljed, vastusEesTaga, vastus;
 		vastusKyljed=4*(korgus*sygavus);
 		vastusEesTaga=2*(laius*korgus);
 		vastus=vastusKyljed+vastusEesTaga;
 		return vastus;
 	}
 	
+	public static double tvPindalaRuutmeeter(double korgus, double laius, double sygavus){
+		double vastusKyljed, vastusEesTaga, vastus, teisendus;
+		vastusKyljed=4*(korgus*sygavus);
+		vastusEesTaga=2*(laius*korgus);
+		vastus=vastusKyljed+vastusEesTaga;
+		teisendus=vastus/10000;
+		return teisendus;
+	}
+	
+	
 	public static void main(String[] args){	
-		int korgus=50;
-		int laius=100;
-		int sygavus=8;
-		System.out.println("Televiisori pindala: "+ tvPindala(korgus, laius, sygavus)+" ruutmeetrit");
+		double korgus=50;
+		double laius=100;
+		double sygavus=8;
+		System.out.println("Televiisori pindala: "+ tvPindala(korgus, laius, sygavus)+" ruutsentimeetrit");
+		System.out.println("Televiisori pindala: "+ tvPindalaRuutmeeter(korgus, laius, sygavus)+" ruutmeetrit");
 	}
 }
