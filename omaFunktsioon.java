f31cc63632af0150a0d5a8f083c73a99179f7ee5
stivsire <stivsire@greeny.cs.tlu.ee>
2017-02-10 12:29:57 +0200

    ruudu pindala asendatud tv pindalaga

--- omaFunktsioon.java
+++ omaFunktsioon.java
@@ -1,8 +1,16 @@
 public class omaFunktsioon{
-	public static double ruudupindala(double kylg){
-		return Math.pow(kylg, 2);
+	public static int tvPindala(int korgus, int laius, int sygavus){
+		int vastusKyljed, vastusEesTaga, vastus;
+		vastusKyljed=4*(korgus*sygavus);
+		vastusEesTaga=2*(laius*korgus);
+		vastus=vastusKyljed+vastusEesTaga;
+		return vastus;
 	}
-	public static void main(String[] args){		
-		System.out.println(ruudupindala(4));
+	
+	public static void main(String[] args){	
+		int korgus=50;
+		int laius=100;
+		int sygavus=8;
+		System.out.println("Televiisori pindala: "+ tvPindala(korgus, laius, sygavus)+" ruutmeetrit");
 	}
 }
