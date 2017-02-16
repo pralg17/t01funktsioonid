f063fec190016c61b2b4fc2be2bd03f101b5434c
egenoor <egenoor@github.com>
2017-02-17 01:30:14 +0200

    added functions

--- kodutoo1.java
+++ kodutoo1.java
@@ -1,26 +1,35 @@
 public class kodutoo1{
-	public static void main(String[] args){
-        int number1 = 10;
-        int number2 = 5;
 
+	public static int summa(int number1, int number2){
         //calculating number1 + number2;
-        int summa = number1 + number2;
+        return number1 + number2;
+	}
+    public static int vahe(int number1, int number2){   //calculating number1 - number2;
+        
+		return number1 - number2;
+		
+	}
+    public static int korruta(int number1, int number2){    //calculating number1 * number2;
 
-        //calculating number1 - number2;
-        int vahe = number1 - number2;
-
-        //calculating number1 * number2;
-        int korrutis = number1 * number2;
-
+		return number1 * number2;
+		
+	}
+	public static double jaga(int number1, int number2){
         //calculating number1 / number2;
-        int jagatis = number1 / number2;
-
-        //Displaying the values
-        System.out.println("number1 : "+number1);
-        System.out.println("number2 : "+number2);
-        System.out.println("summa : "+summa);
-        System.out.println("vahe : "+vahe);
-        System.out.println("korrutis : "+korrutis);
-        System.out.println("jagatis : "+jagatis);
+        return number1 / number2;
+		
+	}
+		
+	public static void main(String[] args){
+		
+		int number1 = 10;
+		int number2 = 5;
+		//Displaying the values
+		System.out.println("number1 : "+number1);
+		System.out.println("number2 : "+number2);
+		System.out.println("summa : "+summa(number1, number2));
+		System.out.println("vahe : "+vahe(number1, number2));
+		System.out.println("korrutis : "+korruta(number1, number2));
+		System.out.println("jagatis : "+jaga(number1, number2));
     }
 }
