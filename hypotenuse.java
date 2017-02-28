50d952519f31303d339b54bf473b6d5cb91d90b5
GittanKaus <GittanKaus@users.noreply.github.com>
2017-02-28 16:38:52 +0200

    Delete hypotenuse.java

--- hypotenuse.java
+++
@@ -1,9 +0,0 @@
-public class hypotenuse {
- 
-    public static void main(String[] args) {
-         
-        int x = 10;
-        int y = 20;
-        System.out.println("Hypotenuse lenght is: "+Math.hypot(x, y));
-    }
-}
