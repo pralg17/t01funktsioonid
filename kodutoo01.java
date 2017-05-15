a20fd0b324239c93913eff35b424bc635ff46184
kkriiska <kkriiska@users.noreply.github.com>
2017-05-15 13:06:33 +0300

    Lisatud kodutöö 1

---
+++ kodutoo01.java
@@ -0,0 +1,14 @@
+public class kodutoo01{
+	public static void trykiA(){
+		System.out.println("K");
+		System.out.println("A");
+		System.out.println("R");
+		System.out.println("O");
+		System.out.println("L");
+		System.out.println("I");
+		System.out.println("N");
+	}
+	public static void main(String[] args){
+		trykiA();
+	}
+}
