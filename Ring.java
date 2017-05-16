ab7d6cfbb6b780b8d57609f4d48ae8407e98d201
Kirill Kotkas <kirillk@tlu.ee>
2017-05-16 19:06:07 +0300

    tested

--- Ring.java
+++ Ring.java
@@ -1,24 +1,18 @@
 /** 
-
-
-*PROGRAMM ARVUTAB RINGI PINDALA
-
-
+PROGRAMM ARVUTAB RINGI PINDALA
 */
 
 
 public class Ring{
 	public static double pindala(){
-		int raadius = 3;
+		int raadius = 15;
 		double pindala = 3.14159 * (raadius * raadius);
 			return pindala;
 		
 	}
 	 
-	 
-	
 	public static void main(String[] args){
-		System.out.println("Ringi pindala on umbkaudu " + pindala());
+		System.out.println("Ringi pindala on" + pindala());
 	
 		
 	}
@@ -26,7 +20,5 @@ public class Ring{
 }
 
 /**
-
-Ringi pindala on umbkaudu 28.27431
-
+Ringi pindala on 706.85775
 */
